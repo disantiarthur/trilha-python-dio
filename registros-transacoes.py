86a2102f71a81d5f783f87b88c707f06a5c4719d
Arthur <disantiarthur@gmail.com>
2025-06-15 13:08:29 -0300

    Create registros-transacoes.py

---
+++ registros-transacoes.py
@@ -0,0 +1,28 @@
+def calcular_saldo(transacoes):
+    saldo = 0
+
+
+    # Itera sobre cada transação na lista:
+    for transacao in transacoes:
+        # Adiciona o valor da transação ao saldo
+        saldo += transacao
+
+
+    # Retorna o saldo formatado em moeda brasileira com duas casas decimais:
+    return f"Saldo: R$ {saldo:.2f}"
+
+
+entrada_usuario = input()
+
+
+entrada_usuario = entrada_usuario.strip("[]").replace(" ", "")
+
+
+transacoes = [float(valor) for valor in entrada_usuario.split(",")]
+
+
+# Calcula o saldo com base nas transações informadas:
+resultado = calcular_saldo(transacoes)
+
+
+print(resultado)
